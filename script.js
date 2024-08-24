4dc7a3392951e954bd820a54b177ace587796e6e
Oxide Dev <129216187+OxideOnly@users.noreply.github.com>
2024-08-24 21:11:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 123) {
+        event.preventDefault();
+    }
+
+    if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
+        event.preventDefault();
+    }
+
+    if (event.ctrlKey && event.keyCode === 85) {
+        event.preventDefault();
+    }
+});
+
+document.querySelector('.search-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var searchInput = document.querySelector('.search-input').value.trim();
+
+    var urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+
+    if (urlPattern.test(searchInput)) {
+        if (!searchInput.startsWith('http://') && !searchInput.startsWith('https://')) {
+            searchInput = 'http://' + searchInput;
+        }
+        window.location.href = searchInput;
+    } else {
+        this.submit(); 
+    }
+});
+
+function updateDateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var day = now.getDate();
+    var month = now.toLocaleString('tr-TR', { month: 'long' });
+    var year = now.getFullYear();
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    var timeString = hours + ':' + minutes;
+    var dateString = day + ' ' + month + ' ' + year;
+
+    document.getElementById('datetime').innerHTML = '<span class="time">' + timeString + '</span>' + '<span class="date">' + dateString + '</span>';
+}
+
+setInterval(updateDateTime, 1000);
+
+updateDateTime();
+
+window.addEventListener('load', function() {
+    document.querySelector('.search-input').value = '';
+});
